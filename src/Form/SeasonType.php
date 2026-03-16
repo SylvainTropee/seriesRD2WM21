3354d0be033c3bdcc5bc467e2b835b8072269a50
stropee <stropee@campus-eni.fr>
2026-03-16 14:45:55 +0100

    route ajout season sur série

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Season;
 use App\Entity\Serie;
+use App\Repository\SerieRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,11 +20,15 @@ class SeasonType extends AbstractType
             ->add('overview')
             ->add('poster')
             ->add('tmdbId')
+//            ->add('serie', SerieType::class)
             ->add('serie', EntityType::class, [
                 'class' => Serie::class,
                 'choice_label' => 'name',
-            ])
-        ;
+                'placeholder' => 'Choisir une série',
+                'query_builder' => function (SerieRepository $serieRepository) {
+                    return $serieRepository->createQueryBuilder('s')->addOrderBy('s.name');
+                }
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
