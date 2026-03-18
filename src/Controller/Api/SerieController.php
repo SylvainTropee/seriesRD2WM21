1eb4969d7efb45105167fd53c2700820c1415a37
stropee <stropee@campus-eni.fr>
2026-03-18 14:31:16 +0100

    ajout post  + delete

--- src/Controller/Api/SerieController.php
+++ src/Controller/Api/SerieController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Serie;
 use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/series', name: 'api_series_')]
 final class SerieController extends AbstractController
@@ -21,13 +25,14 @@ final class SerieController extends AbstractController
         $series = $serieRepository->findAll();
         //dd($serializer->serialize($series, 'json', ['groups' => 'serie-api']));
         return $this->json($series, Response::HTTP_OK, [], ['groups' => 'serie-api']);
-
     }
 
     #[Route('/{id}', name: 'retrieve_one', methods: 'GET')]
-    public function retrieveOne(int $id): Response
+    public function retrieveOne(int $id, SerieRepository $serieRepository): Response
     {
         //TODO renvoyer une série au format json
+        $serie = $serieRepository->find($id);
+        return $this->json($serie, Response::HTTP_OK, [], ['groups' => 'serie-api']);
     }
 
     #[Route('/{id}', name: 'update', methods: ['PUT', 'PATCH'])]
@@ -37,16 +42,38 @@ final class SerieController extends AbstractController
     }
 
     #[Route('', name: 'create', methods: ['POST'])]
-    public function create(): Response
+    public function create(
+        SerializerInterface $serializer,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        ValidatorInterface $validator
+    ): Response
     {
-        //TODO ajotuer une nouvelle série
+        $json = $request->getContent();
+        $serie = $serializer->deserialize($json, Serie::class, 'json');
+
+        //validation des données, comme pour un formulaire
+        $errors = $validator->validate($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        return $this->json($errors, Response::HTTP_CREATED, [], ['groups' => 'serie-api']);
     }
 
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
-    public function delete(int $id): Response
+    public function delete(
+        int $id,
+        SerieRepository $serieRepository,
+        EntityManagerInterface $entityManager
+    ): Response
     {
-        //TODO supprimer une série
+        $serie = $serieRepository->find($id);
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+        return $this->json(['success' => 'Serie deleted !'], Response::HTTP_ACCEPTED);
     }
 
 
